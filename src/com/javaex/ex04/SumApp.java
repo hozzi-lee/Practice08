8707c22938dba4db67e5dd8fd50e680c6f311366
leehojin-93 <hogenie93@gmail.com>
2021-06-11 22:44:53 +0900

    Practice08_incomplete

---
+++ src/com/javaex/ex04/SumApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class SumApp {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 공백으로 구분해서 입력하세요");
+		String numLine = sc.nextLine();
+
+		//코드를 작성하세요
+		String[] numArray = numLine.split(" ");
+		for ( int i = 0; i < numArray.length; i++ ) {
+			System.out.println(numArray[i]);
+			sum += numArray[i]; // incomplete
+		}
+
+
+
+		System.out.println("합:" + sum);
+
+		sc.close();
+	}
+
+}
