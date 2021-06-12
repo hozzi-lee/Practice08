e7af0583b02dd11a7253788f027be1daf442cc3c
leehojin-93 <hogenie93@gmail.com>
2021-06-12 22:59:16 +0900

    Practice08_incomplete2

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -15,7 +15,7 @@ public class SumApp {
 		String[] numArray = numLine.split(" ");
 		for ( int i = 0; i < numArray.length; i++ ) {
 			System.out.println(numArray[i]);
-			sum += numArray[i]; // incomplete
+			sum += (int)(numArray[i].charAt(0)); // incomplete
 		}
 
 
